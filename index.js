ac824ad9d3aa536acf1ad3783d54b34be74afc81
Jacob Smith <jacob@jacobsmith.io>
2017-09-25 15:35:58 -0400

    Add support for passing options through to js-yaml

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+var cloneDeep = require('lodash.clonedeep');
+var loaderUtils = require('loader-utils');
 var yaml = require('js-yaml');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
+  var options = cloneDeep(loaderUtils.getOptions(this) || {});
   try {
-    var res = yaml.safeLoad(source);
+    var res = yaml.safeLoad(source, options);
     return JSON.stringify(res, undefined, '\t');
   }
   catch (err) {
